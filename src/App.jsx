6031825b73f0fe4c0599fac1a060306eadcbd6a6
BeyonderCrypto <alfonso.segoviam@gmail.com>
2023-05-25 13:45:00 -0600

    se creo la lista de criptomonedas, se agrego axios (bueno)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function App() {
+const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos,setCriptos] = useState()
+
+  // VERSION SIN AXIOS
+  /*useEffect(() => {
+    fetch(`${API_URL}assets`)
+    .then((resp) => resp.json() )
+    .then((data) => {
+      setCriptos(data.data)
+      console.log(data)
+    })
+    .catch(() => {
+      console.error("la petición de datos al API de coincap falló.")
+    })
+  }, [])*/
+
+  // VERSION CON AXIOS
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+    .then((data) => {
+      setCriptos(data.data.data)
+      console.log(data.data.data)
+    })
+    .catch(() => {
+      console.error("la petición de datos al API de coincap falló.")
+    })
+  }, [])
+
+  if (!criptos) return <span>Cargando...</span>
   return (
-      <h1>Saludos Ponch</h1>
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {
+          criptos.map(({id, name, priceUsd}) => (
+            <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+          ))
+        }
+      </ol>
+    </>  
   )
 }
 
